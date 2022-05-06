882ede319e247a13256f7a9947c907531be0bf9b
fernandagac05 <one.mafer@gmail.com>
2022-05-05 21:30:18 -0500

    Cambios clase 12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,18 @@ use App\Models\Posts;
 
 class PostController extends Controller
 {
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    
     /**
      * Display a listing of the resource.
      *
